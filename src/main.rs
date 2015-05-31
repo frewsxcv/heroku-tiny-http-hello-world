6bf6d4c56ec7a80710ac3e39c260c4cd9021a609
Corey Farwell <coreyf@rwell.org>
2015-05-31 10:03:11 -0400

    Add hello world server

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,20 @@
+extern crate tiny_http;
+
+use std::env;
+
+use tiny_http::{Response, ServerBuilder};
+
+
 fn main() {
+    let port = match env::var("PORT") {
+        Ok(p) => p.parse::<u16>().unwrap(),
+        Err(..) => 8000,
+    };
+
+    let server = ServerBuilder::new().with_port(port).build().unwrap();
+
+    for req in server.incoming_requests() {
+        let response = Response::from_string("Hello world!");
+        req.respond(response);
+    }
 }
